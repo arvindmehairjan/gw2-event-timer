178d9c966cc584c699a6bffb20c3ea0f48d2c62e
Arvind <mehairjanarvind@gmail>
2023-12-20 23:26:27 +0100

    fixing logic

--- gw2-event-timer/src/App.js
+++ gw2-event-timer/src/App.js
@@ -1,105 +1,17 @@
-// Importing necessary dependencies and files
 import React, { useState, useEffect } from 'react';
-import jsonData from './data/events.json'; // Importing event data from a JSON file
-import './App.css'; // Importing styles
+import jsonData from './data/events.json'; 
+import './App.css';
 
-// Functional component definition
 const App = () => {
-  // State variables using the useState hook
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
-  const [upcomingEvents, setUpcomingEvents] = useState([]);
 
-  // useEffect hook for handling side effects
-  useEffect(() => {
-    // Interval to update the current time every second
-    const intervalId = setInterval(() => {
-      setCurrentTime(new Date().toLocaleTimeString());
-    }, 1000);
 
-    // Extracting event data for 'Core Tyria' from the imported JSON
-    const coreTyriaEvent = jsonData['Core Tyria'];
-
-    // Processing the event data to filter upcoming events
-    if (coreTyriaEvent) {
-      const upcomingEventsData = coreTyriaEvent.reduce((accumulator, event) => {
-        // Filtering spawn times within a specific time range
-        const upcomingSpawnTimes = event.spawnTimer.filter(spawnTime => {
-          const spawnDateTime = new Date();
-          const [hours, minutes] = spawnTime.split(':');
-          spawnDateTime.setHours(parseInt(hours, 10));
-          spawnDateTime.setMinutes(parseInt(minutes, 10));
-
-          const timeDifference = spawnDateTime.getTime() - new Date().getTime();
-
-          return timeDifference > -30 * 60 * 1000 && timeDifference <= 2 * 60 * 60 * 1000;
-        });
-
-        // Filtering and processing events within the specified time range
-        if (upcomingSpawnTimes.length > 0) {
-          accumulator.push({
-            bossName: event.bossName,
-            nextSpawnTime: upcomingSpawnTimes.reduce((earliest, spawnTime) => {
-              const spawnDateTime = new Date();
-              const [hours, minutes] = spawnTime.split(':');
-              spawnDateTime.setHours(parseInt(hours, 10));
-              spawnDateTime.setMinutes(parseInt(minutes, 10));
-
-              const timeDifference = spawnDateTime.getTime() - new Date().getTime();
-
-              return timeDifference < earliest ? timeDifference : earliest;
-            }, Infinity),
-            countdowns: upcomingSpawnTimes.map(spawnTime => {
-              const spawnDateTime = new Date();
-              const [hours, minutes] = spawnTime.split(':');
-              spawnDateTime.setHours(parseInt(hours, 10));
-              spawnDateTime.setMinutes(parseInt(minutes, 10));
-
-              const timeDifference = spawnDateTime.getTime() - new Date().getTime();
-
-              const isActive = timeDifference <= 0 && timeDifference > -30 * 60 * 1000;
-              const remainingTime = isActive ? 30 * 60 * 1000 + timeDifference : timeDifference;
-
-              const hoursRemaining = Math.floor(remainingTime / (1000 * 60 * 60));
-              const minutesRemaining = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-
-              return isActive ? 'Active' : `${hoursRemaining}h ${minutesRemaining}m`;
-            }),
-          });
-        }
-
-        return accumulator;
-      }, []);
-
-      // Sort the upcoming events based on the nextSpawnTime
-      upcomingEventsData.sort((a, b) => a.nextSpawnTime - b.nextSpawnTime);
-
-      // Setting state with the processed upcoming events
-      setUpcomingEvents(upcomingEventsData);
-    }
-
-    // Clearing the interval when the component is unmounted
-    return () => clearInterval(intervalId);
-
-    // Dependencies for the useEffect hook
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentTime, new Date()]);
-
-  // JSX for rendering the component
   return (
     <div className="App">
       <h1>Guild Wars 2 - Meta Event Timer</h1>
-      <p>Current Time: {currentTime}</p>
-      <p>Upcoming Events in the Next 2 Hours:</p>
-      <ul>
-        {upcomingEvents.map((event, index) => (
-          <li key={index}>
-            {event.bossName} - {event.countdowns.join(', ')}
-          </li>
-        ))}
-      </ul>
+      <p>Current Time: </p>
+      <p>Closest Upcoming Event:</p>
     </div>
   );
 };
 
-// Exporting the component as the default export
 export default App;
