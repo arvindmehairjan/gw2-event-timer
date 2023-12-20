2e7727788c707513d42bb1ccef581b07965c9619
Arvind <mehairjanarvind@gmail>
2023-12-20 16:36:03 +0100

    Added current timer

--- gw2-event-timer/src/App.js
+++ gw2-event-timer/src/App.js
@@ -1,11 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const today = new Date();
+  const [currentTime, setCurrentTime] = useState(today.toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
-      <h1>gw2 event timer</h1>
+      <h1>Guild Wars 2 - Meta Event Timer</h1>
+      <p>Current Time: {currentTime}</p>
     </div>
   );
-}
+};
 
 export default App;
