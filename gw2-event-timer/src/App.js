63d4670530569082804e6632c6109e1f2dc1c240
Arvind <mehairjanarvind@gmail>
2023-12-20 16:58:51 +0100

    Added countdown

--- gw2-event-timer/src/App.js
+++ gw2-event-timer/src/App.js
@@ -3,12 +3,12 @@ import jsonData from './data/events.json';
 import './App.css';
 
 const App = () => {
-  const today = new Date();
-  const [currentTime, setCurrentTime] = useState(today.toLocaleTimeString());
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
   const [bossInfo, setBossInfo] = useState({
     bossName: '',
     spawnTimer: ''
   });
+  const [countdown, setCountdown] = useState('');
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -18,21 +18,35 @@ const App = () => {
     const coreTyriaEvent = jsonData['Core Tyria'];
 
     if (coreTyriaEvent) {
+      const spawnTime = new Date();
+      const [hours, minutes] = coreTyriaEvent.spawnTimer.split(':');
+      spawnTime.setHours(parseInt(hours, 10));
+      spawnTime.setMinutes(parseInt(minutes, 10));
+
+      const timeDifference = spawnTime.getTime() - new Date().getTime();
+
+      // Calculate remaining hours and minutes
+      const hoursRemaining = Math.floor(timeDifference / (1000 * 60 * 60));
+      const minutesRemaining = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+
       setBossInfo({
         bossName: coreTyriaEvent.bossName,
         spawnTimer: coreTyriaEvent.spawnTimer
       });
+
+      setCountdown(`${hoursRemaining}h ${minutesRemaining}m`);
     }
 
     return () => clearInterval(intervalId);
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentTime, new Date()]);
 
   return (
     <div className="App">
       <h1>Guild Wars 2 - Meta Event Timer</h1>
       <p>Current Time: {currentTime}</p>
       <p>Boss Name: {bossInfo.bossName}</p>
-      <p>Spawn Timer: {bossInfo.spawnTimer}</p>
+      <p>Countdown: Event start in {countdown}</p>
     </div>
   );
 };
