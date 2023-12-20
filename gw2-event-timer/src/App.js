b254cdc476d86e2654a1dd48aed89609f431bba0
Arvind <mehairjanarvind@gmail>
2023-12-20 23:31:59 +0100

    fix timer

--- gw2-event-timer/src/App.js
+++ gw2-event-timer/src/App.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import jsonData from './data/events.json'; 
+import jsonData from './data/events.json';
 import './App.css';
 
 const App = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  })
 
   return (
     <div className="App">
       <h1>Guild Wars 2 - Meta Event Timer</h1>
-      <p>Current Time: </p>
+      <p>Current Time: {currentTime.toLocaleTimeString()} </p>
       <p>Closest Upcoming Event:</p>
     </div>
   );
