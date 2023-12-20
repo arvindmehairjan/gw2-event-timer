4fe7ff71322cf109c206c90d006acea67c813e3c
Arvind <mehairjanarvind@gmail>
2023-12-20 23:34:45 +0100

    shows jsonData

--- gw2-event-timer/src/App.js
+++ gw2-event-timer/src/App.js
@@ -11,13 +11,26 @@ const App = () => {
     }, 1000);
 
     return () => clearInterval(interval);
-  })
+  }, []);
 
   return (
     <div className="App">
       <h1>Guild Wars 2 - Meta Event Timer</h1>
       <p>Current Time: {currentTime.toLocaleTimeString()} </p>
-      <p>Closest Upcoming Event:</p>
+      <p>Upcoming Events:</p>
+
+      {Object.keys(jsonData).map((region, regionIndex) => (
+        <div key={regionIndex}>
+          <h2>{region}</h2>
+          <ul>
+            {jsonData[region].map((event, eventIndex) => (
+              <li key={eventIndex}>
+                <strong>{event.bossName}</strong> - Spawn Timer: {event.spawnTimer.join(', ')}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
