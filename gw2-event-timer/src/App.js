08f202001d52e7667c3b76012f902ec8de1cf296
Arvind <mehairjanarvind@gmail>
2023-12-21 00:12:06 +0100

    Added method to calculate time remaining

--- gw2-event-timer/src/App.js
+++ gw2-event-timer/src/App.js
@@ -3,23 +3,48 @@ import jsonData from './data/events.json';
 import './App.css';
 
 const App = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const date = new Date();
+  const currentTime = date.getHours()
+      + ':' + date.getMinutes()
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
+  const firstArray = jsonData['Core Tyria'][0];
+  const bossName = firstArray.bossName;
+  const spawnTimer = firstArray.spawnTimer;
+  console.log(spawnTimer);
 
-    return () => clearInterval(interval);
-  }, []);
+  console.log(bossName);
+
+  const timeDifference = () => {
+    const now = new Date();
+
+    // Set the target time (02:04) for today
+    const targetTime = new Date();
+    targetTime.setHours(2, 4, 0, 0);
+
+    // Calculate the time difference in milliseconds
+    const timeDiff = targetTime - now;
+
+    // Check if the target time is in the future
+    if (timeDiff > 0) {
+      // Calculate hours and minutes
+      const hours = Math.floor(timeDiff / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+
+      // Display the remaining time
+      console.log(`Time remaining: ${hours} hours and ${minutes} minutes`);
+    } else {
+      console.log("The target time has already passed for today.");
+    }
+  }
 
   return (
     <div className="App">
       <h1>Guild Wars 2 - Meta Event Timer</h1>
-      <p>Current Time: {currentTime.toLocaleTimeString()} </p>
+      <p>Current Time: {currentTime} </p>
       <p>Upcoming Events:</p>
+      <p>{timeDifference()}</p>
 
-      
+
     </div>
   );
 };
