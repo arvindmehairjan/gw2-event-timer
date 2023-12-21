b1f0624aea8fb574d4b11b48d315f5535b2bae47
Arvind <mehairjanarvind@gmail>
2023-12-21 13:04:05 +0100

    Added hero banner

--- gw2-event-timer/src/App.js
+++ gw2-event-timer/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import TableView from './components/Table';
 import NavBar from './components/Navbar';
+import Hero from './components/Hero';
 const App = () => {
 
   return (
     <div className="App">
       <NavBar/>
-      <h1>Guild Wars 2 - Meta Event Timer</h1>
+      <Hero/>
       <TableView region="Core Tyria"/>
     </div>
   );
---
+++ gw2-event-timer/src/components/Hero.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div class="bg-blue-500 text-white h-screen flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold mb-4">Guild Wars 2 - Meta Event Timer</h1>
+      <h2 class="text-2xl font-semibold">Event timers of meta events</h2>
+    </div>
+  </div>
+  );
+};
+
+export default Hero;
