90052d619f27ace43d81c5dcba44cf7a9cd43e96
Arvind <mehairjanarvind@gmail>
2023-12-21 13:25:26 +0100

    changed class to className

--- gw2-event-timer/src/components/Hero.js
+++ gw2-event-timer/src/components/Hero.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div class="bg-blue-500 text-white h-screen flex items-center justify-center">
-    <div class="text-center">
-      <h1 class="text-4xl font-bold mb-4">Guild Wars 2 - Meta Event Timer</h1>
-      <h2 class="text-2xl font-semibold">Event timers of meta events</h2>
+    <div className="bg-blue-500 text-white h-screen flex items-center justify-center">
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">Guild Wars 2 - Meta Event Timer</h1>
+      <h2 className="text-2xl font-semibold">Event timers of meta events</h2>
     </div>
   </div>
   );
