278d41dce2e275673edf5994d9543abc9e692536
Arvind <mehairjanarvind@gmail>
2023-12-22 22:10:03 +0100

    changed config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import './src/components/*.js'
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/*.js",
+    "./src/pages/*.js"
   ],
   theme: {
     extend: {},
