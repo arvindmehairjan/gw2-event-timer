fc3c7a4e0832fecf6f7b7585695cb0e77107a392
Arvind <mehairjanarvind@gmail>
2023-12-22 16:13:53 +0100

    Added more link elements on home page

--- src/components/EventBtn.js
+++ src/components/EventBtn.js
@@ -3,27 +3,57 @@ import { Link } from 'react-router-dom';
 
 const EventBtn = () => {
   return (
-    <div className="flex mt-5 mb-3"> {/* Added flex and margin for spacing */}
-      <Link
-        to="/core-tyria"
-        className="bg-red-500 hover:bg-red-700 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mr-3"
-      >
-        Core Tyria Events
-      </Link>
+    <div className="flex flex-col items-center mt-5 mb-3">
+      <div className="flex flex-wrap justify-center">
+        <Link
+          to="/core-tyria"
+          className="bg-red-500 hover:bg-red-700 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mb-3 mr-2"
+        >
+          Core Tyria
+        </Link>
 
-      <Link
-        to="/living-world"
-        className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mr-3"
-      >
-        Living World Events
-      </Link>
+        <Link
+          to="/living-world"
+          className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mb-3 mr-2"
+        >
+          Living World
+        </Link>
 
-      <Link
-        to="/living-world"
-        className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out"
-      >
-        Living World Events
-      </Link>
+        <Link
+          to="/heart-of-thorns"
+          className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mb-3 mr-2"
+        >
+          Heart of Thorns
+        </Link>
+
+        <Link
+          to="/path-of-fire"
+          className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mb-3 mr-2"
+        >
+          Path of Fire
+        </Link>
+
+        <Link
+          to="/icebrood-saga"
+          className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mb-3 mr-2"
+        >
+          Icebrood Saga
+        </Link>
+
+        <Link
+          to="/end-of-dragons"
+          className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mb-3 mr-2"
+        >
+          End of Dragons
+        </Link>
+
+        <Link
+          to="/secrets-of-obscure"
+          className="bg-neutral-600 hover:bg-neutral-900 text-white px-3 py-1 rounded inline-block transition duration-300 ease-in-out mb-3 mr-2"
+        >
+          Secrets Of The Obscure
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <div className="bg-white-500 text-black h-screen flex items-center justify-center">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Guild Wars 2 - Meta Event Timer</h1>
-        <h2 className="text-1.5xl font-semibold">Discover the schedules for all meta events and world bosses.</h2>
+        <h1 className="text-4xl font-bold mb-4">Meta Event Timer</h1>
+        <h2 className="text-1xl font-semibold">Schedules for all meta events and world bosses.</h2>
         <EventBtn/>
 
       </div>
