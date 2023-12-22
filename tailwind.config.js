0d92ed18ac78f48764c5653a9f8ac8b1b8422604
Arvind <mehairjanarvind@gmail>
2023-12-22 21:20:24 +0100

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
+    './src/components/*.{js,jsx,ts,tsx,vue}',
+    './src/pages/*.{js,jsx,ts,tsx,vue}',
   ],
   // other Tailwind CSS configurations go here
 };
